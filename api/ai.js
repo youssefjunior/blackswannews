57828ecdce0b8383f289fba41851b89fdba717bc
youssefjunior <youssefjunior@proton.me>
2026-05-28 08:57:34 -0300

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
         'Authorization': `Bearer ${process.env.GROQ_API_KEY}`
       },
       body: JSON.stringify({
-        model: 'llama3-8b-8192',
+       model: 'llama-3.3-70b-versatile',
         max_tokens: max_tokens || 900,
         messages: [{ role: 'user', content: prompt }]
       }),
